79e28616bada43b7205cd776fe350e31f116d766
bhaskarchinta <bhaskarc@SMBXL.COM>
2025-10-31 17:07:28 +0530

    bug fix for master data

--- src/Swachify.Application/Helpers/DbConstants.cs
+++ src/Swachify.Application/Helpers/DbConstants.cs
@@ -9,7 +9,8 @@ public static class DbConstants
     public readonly static string fn_get_all_masters_data = "SELECT * FROM fn_get_all_masters_data()";
 
     public readonly static string fn_service_booking_get_list_by_booking_id = "SELECT * FROM fn_service_booking_list(tickt_id:={0})";
-    public readonly static string fn_create_master_departments = "SELECT * FROM fn_create_master_departments(p_department_id:=“{department_id}”p_department_name := “{department_name}”,p_service_id:=“{service_id}”,p_service_name := “{service_name}”,p_service_type_id:=“{service_type_id}”p_service_type := “{service_type}”,p_price := “{price}“);";
+    public readonly static string fn_create_master_departments = "SELECT * FROM fn_create_master_departments(p_department_name := '{department_name}',p_service_name := '{service_name}',p_service_type := '{service_type}',p_price := {price})";
+    public readonly static string fn_update_master_departments = "SELECT * FROM fn_create_master_departments(p_department_id:='{department_id}',p_department_name := '{department_name}',p_service_id:='{service_id}',p_service_name := '{service_name}',p_service_type_id:='{service_type_id}',p_service_type := '{service_type}',p_price := {price});";
 
 
 }
